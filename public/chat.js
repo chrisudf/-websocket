fb451f97dcbd956f2d9c4e5262daf1597e068e1b
DESKTOP-OFH8I45\Qiu <chengqiu03@gmail.com>
2019-04-08 10:22:50 +1000

    live chat using websocket

---
+++ public/chat.js
@@ -0,0 +1,38 @@
+//make connection
+var socket =io.connect('http://localhost:4000');
+
+//query DOM
+var message=document.getElementById('message')
+handle=document.getElementById('handle')
+btn=document.getElementById('send')
+output=document.getElementById('output')
+feedback=document.getElementById('feedback')
+
+//emit event
+
+btn.addEventListener('click',function(){
+    socket.emit('chat',{
+        message:message.value,
+        handle:handle.value
+    })
+    // message.value = ""
+    // message.innerHTML =""
+})
+
+message.addEventListener('keypress',function(){
+    socket.emit('typing',handle.value);
+})
+
+
+
+//listen for events
+socket.on('chat',function(data){
+    feedback.innerHTML='' //can't delete feedback line by using feedback.value=''
+    output.innerHTML +='<p><strong>'+data.handle +':</strong>'+data.message +'</p>'
+    // message.innerHTML ="" ???
+    message.value = ""
+})
+
+socket.on('typing',function(data){
+    feedback.innerHTML ='<p><em>'+ data + 'is typing a message... </em></p>'
+})
